60ebb826d946e46df75ea4a4286dccd95fb28bd5
DepresslyJuice <103459551+DepresslyJuice@users.noreply.github.com>
2025-01-29 22:54:39 -0500

    Audit en citas y pacientes

--- app/Models/CitaMedica.php
+++ app/Models/CitaMedica.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class CitaMedica extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll() // Guarda todos los atributos
+            ->logOnlyDirty() // Solo guarda cambios reales
+            ->useLogName('citas_medicas')
+            ->dontSubmitEmptyLogs(); // Evita guardar logs vacíos
+    }
 
     /**
      * Tabla asociada al modelo.
@@ -28,6 +39,19 @@ class CitaMedica extends Model
         'consultorio_id',
         'estado_citas_id',
     ];
+
+
+    //Auditoria
+    protected static $logAttributes = ['*']; // Todos los atributos
+    protected static $logOnlyDirty = true; // Solo guarda cambios si son diferentes
+    protected static $logName = 'citas_medicas';
+    protected static $logAttributesToIgnore = ['updated_at']; // Evita cambios irrelevantes
+    protected static $submitEmptyLogs = false; // Evita guardar logs vacíos
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "Una cita médica ha sido {$eventName} por el usuario " . (auth()->user()->name ?? 'Sistema');
+    }
 
     /**
      * Relación con el modelo Paciente.
